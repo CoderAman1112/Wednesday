920357b521e2f09ba27f7378838529019332806d
Aman <mr.aman12nov@gmail.com>
2025-08-02 23:02:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const nextBtn = document.querySelector(".btn-1")
 const gallery = document.querySelector(".images")
 let currentImg = 2
 nextBtn.addEventListener("click", ()=> {
-    images = ['/source/img-2.jpg', '/source/img-3.jpg', '/source/img-4.jpg', '/source/img-5.jpg']
     currentImg += 1
-    gallery.src = `/source/img-${currentImg}.jpg`
+    gallery.src = `./source/img-${currentImg}.jpg`
     
     if(currentImg>4){
         currentImg = 1
     }
+
 })
